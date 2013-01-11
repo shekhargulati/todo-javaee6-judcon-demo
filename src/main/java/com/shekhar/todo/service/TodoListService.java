45b5949dc0533f0b3745db74194172565babc36e
Shekhar Gulati <shekhargulati84@gmail.com>
2013-01-12 00:10:45 +0530

    added event support for TodoList

--- src/main/java/com/shekhar/todo/service/TodoListService.java
+++ src/main/java/com/shekhar/todo/service/TodoListService.java
@@ -3,6 +3,7 @@ package com.shekhar.todo.service;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
@@ -16,10 +17,14 @@ public class TodoListService {
 
 	@Inject
 	Logger logger;
+	
+	@Inject
+	Event<TodoList> todoEvent;
 
 	public TodoList create(TodoList todoList) {
 		logger.info("Persisting TodoList with name " + todoList.getName());
 		entityManager.persist(todoList);
+		todoEvent.fire(todoList);
 		return todoList;
 	}
 }
