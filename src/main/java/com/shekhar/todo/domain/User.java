6275c2eabdefa4fdabc4357266f85bd78b29ee88
Shekhar Gulati <shekhargulati84@gmail.com>
2013-01-11 16:44:30 +0530

    changes User table to Users as in PostgreSQL user is keyword

--- src/main/java/com/shekhar/todo/domain/User.java
+++ src/main/java/com/shekhar/todo/domain/User.java
@@ -14,8 +14,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Users")
 public class User implements Serializable{
 
 	@Id
