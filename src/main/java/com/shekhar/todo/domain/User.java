ea8da82d796e1f58702e864f84394e63b5af182a
Shekhar Gulati <shekhargulati84@gmail.com>
2013-01-11 16:59:23 +0530

    made hobbies eager fetch

--- src/main/java/com/shekhar/todo/domain/User.java
+++ src/main/java/com/shekhar/todo/domain/User.java
@@ -9,6 +9,7 @@ import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -33,7 +34,7 @@ public class User implements Serializable{
 	@OneToMany(mappedBy="createdBy",cascade=CascadeType.ALL)
 	private List<TodoList> todoLists;
 
-	@ElementCollection
+	@ElementCollection(fetch=FetchType.EAGER)
 	@CollectionTable(name = "Hobbies", joinColumns = @JoinColumn(name = "user_id"))
 	@Column(name = "hobby")
 	private List<String> hobbies;
